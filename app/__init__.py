5f3a2618ff38b14031b93022317223085b52280f
ariyoaziz <ariyoaziz.pratama@gmail.com>
2023-12-14 15:16:08 +0700

    commit2

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+# from app import routes
+
+app = Flask(__name__)
+
+from app import routes
+
+# from app import routes
---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from app import app
+
+
+
+@app.route("/")
+def base():
+    return render_template('base.html')
+
+@app.route("/about")
+def about():
+    return render_template('about_us.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/home")
+def home():
+    return render_template('base.html')
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# from app import app
